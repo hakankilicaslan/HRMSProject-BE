09f61594803851d12d45b8005399165a5b88133e
hakankilicaslan <kilicaslanhakan0@gmail.com>
2024-02-28 16:30:52 +0300

    AdminMicroserviceApplication'a yorum eklendi.

--- admin-microservice/src/main/java/org/hrms/AdminMicroserviceApplication.java
+++ admin-microservice/src/main/java/org/hrms/AdminMicroserviceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AdminMicroserviceApplication {
-
+//Hakan Kilicaslan
     public static void main(String[] args) {
         SpringApplication.run(AdminMicroserviceApplication.class, args);
     }
