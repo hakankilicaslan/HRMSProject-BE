bf4033ed79e4740a3e153c86ef33a2c5016f7edb
hakankilicaslan <kilicaslanhakan0@gmail.com>
2024-02-28 17:25:58 +0300

    Projenin güncel hali

--- auth-microservice/src/main/java/org/hrms/AuthMicroserviceApplication.java
+++ auth-microservice/src/main/java/org/hrms/AuthMicroserviceApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Bu anotasyon, bir Spring Boot uygulamasının başlatılmasını sağlayan temel yapıyı oluşturur ve genellikle uygulamanın ana sınıfı üzerinde kullanılır.
  * @SpringBootApplication anotasyonu @SpringBootConfiguration, @EnableAutoConfiguration ve @ComponentScan anotasyonlarının birleşimini temsil eder.
  *
- * main metodumuzun içinde Spring Boot uygulamalarını başlatmak için kullanılan SpringApplication sınıfı üzerinden run metodunu cagirarak baslatilacak olan sinifimizi veriyoruz
+ * main metodumuzun içinde Spring Boot uygulamalarını başlatmak için kullanılan SpringApplication sınıfı üzerinden run metodunu cağırarak başlatılacak olan sınıfımızı veriyoruz.
  * Bu şekilde uygulamanın başlangıç yani giriş noktasını belirtmiş oluyoruz.
  */
 @SpringBootApplication
