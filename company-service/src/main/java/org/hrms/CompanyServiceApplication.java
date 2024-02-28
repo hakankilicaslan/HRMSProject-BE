750b16f4e0089ce39e94992f91480086e9bcf2e1
ozan <celebilerozan@gmail.com>
2024-02-28 16:41:03 +0300

    temp change

--- company-service/src/main/java/org/hrms/CompanyServiceApplication.java
+++ company-service/src/main/java/org/hrms/CompanyServiceApplication.java
@@ -8,6 +8,8 @@ public class CompanyServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CompanyServiceApplication.class, args);
+
+        // test deneme ozan
     }
 
 }
